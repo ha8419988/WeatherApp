a0fcc61d7ac0cdc28c5b8a5731fdaaf70c27c55c
ADMIN <hoanganh8498@gmail.com>
2021-02-02 09:41:59 +0700

    xoa Class Rain

--- app/src/main/java/com/example/test/Model/MyList.java
+++ app/src/main/java/com/example/test/Model/MyList.java
@@ -2,7 +2,6 @@ package com.example.test.Model;
 
 import com.example.test.Common.Clouds;
 import com.example.test.Common.Main;
-import com.example.test.Common.Rain;
 import com.example.test.Common.Sys;
 import com.example.test.Common.Weather;
 import com.example.test.Common.Wind;
@@ -16,6 +15,5 @@ public class MyList {
     public Wind wind;
     public Sys sys;
     public Main main;
-    public Rain rain;
 
 }
